75ee96f93362c1d1f1fa1eadb7c4854d7dcd1173
thibault <thibault402@hotmail.fr>
2014-10-25 00:26:43 +0200

    update vote works perfectly

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -24,21 +24,6 @@ class VotesController < ApplicationController
         redirect_to reference_path(comment.reference_id)
       else
         @my_vote = Vote.where({user_id: current_user.id, reference_id: vote_params[:reference_id], field: vote_params[:field], value: vote_params[:value]}).first
-        if vote_params[:value] == "1"
-          Comment.decrement_counter( :votes_plus, @my_vote.comment_id)
-          Comment.decrement_counter( :rank, @my_vote.comment_id)
-          Comment.increment_counter( :votes_plus,vote_params[:comment_id])
-          Comment.increment_counter( :rank,vote_params[:comment_id])
-        elsif vote_params[:value] == "0"
-          Comment.decrement_counter(:votes_minus, @my_vote.comment_id)
-          Comment.increment_counter(:rank, @my_vote.comment_id)
-          Comment.increment_counter(:votes_minus, vote_params[:comment_id])
-          Comment.decrement_counter(:rank, vote_params[:comment_id])
-        end
-        most = Comment.where(reference_id: vote_params[:reference_id], field: vote_params[:field]).order(rank: :desc).first
-        if most.reference.field_id( vote_params[:field] ) != most.id
-          most.reference.displayed_comment( most )
-        end
         if @my_vote.update( {comment_id: vote_params[:comment_id]})
           flash[:success] = "Vote enregistré"
           redirect_to controller: 'references', action: 'show', id: vote_params[:reference_id]
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,7 @@ class Vote < ActiveRecord::Base
   belongs_to :comment
 
   after_create  :cascading_save_vote
+  around_update  :cascading_update_vote
 
   validates :user_id, presence: true
   validates :timeline_id, presence: true
@@ -14,7 +15,6 @@ class Vote < ActiveRecord::Base
   validates :field, presence: true
   validates_uniqueness_of :user_id, :scope => [:reference_id, :field, :value]
   validates_uniqueness_of :user_id, :scope => [:comment_id, :value]
-
 
   private
 
@@ -32,5 +32,25 @@ class Vote < ActiveRecord::Base
     end
     Reference.increment_counter(:nb_votes, self.reference_id)
     Timeline.increment_counter(:nb_votes, self.timeline_id)
+  end
+
+  def cascading_update_vote
+    old_comment_id = self.comment_id_was
+    yield
+    if self.value == 1
+      Comment.decrement_counter( :votes_plus, old_comment_id)
+      Comment.decrement_counter( :rank, old_comment_id)
+      Comment.increment_counter( :votes_plus, self.comment_id)
+      Comment.increment_counter( :rank, self.comment_id)
+    elsif self.value == 0
+      Comment.decrement_counter(:votes_minus, old_comment_id)
+      Comment.increment_counter(:rank, old_comment_id)
+      Comment.increment_counter(:votes_minus, self.comment_id)
+      Comment.decrement_counter(:rank, self.comment_id)
+    end
+    most = Comment.where(reference_id: self.reference_id, field: self.field ).order(rank: :desc).first
+    if most.reference.field_id( self.field ) != most.id
+      most.reference.displayed_comment( most )
+    end
   end
 end
