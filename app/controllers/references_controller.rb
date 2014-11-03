bafe2585535c9ae2ef125af8094960189d785ab6
thibault <thibault402@hotmail.fr>
2014-11-03 18:31:48 +0100

    Added the progress bar in reference show for number of stars (rating references)

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -70,6 +70,31 @@ class ReferencesController < ApplicationController
       @comments_user[fi] = @reference.comments.where(field: fi, user: current_user.id).first
       end
     end
+    sum = @reference.star_1+@reference.star_2+@reference.star_3+@reference.star_4+@reference.star_5
+    if sum != 0
+      @rating = {}
+      @rating[1] = @reference.star_1*100/sum
+      @rating[2] = @reference.star_2*100/sum
+      @rating[3] = @reference.star_3*100/sum
+      @rating[4] = @reference.star_4*100/sum
+      @rating[5] = @reference.star_5*100/sum
+    else
+      @rating = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, panel: "default" }
+    end
+    case @reference.star_most
+      when 1
+        @rating[:panel] = "danger"
+      when 2
+        @rating[:panel] = "warning"
+      when 3
+        @rating[:panel] = "success"
+      when 4
+        @rating[:panel] = "info"
+      when 5
+        @rating[:panel] = "primary"
+      else
+        @rating[:panel] = "default"
+    end
   end
 
   private
