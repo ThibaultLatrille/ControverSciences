89ee3a07d1b812221de467e9e1d98429594d54a4
thibault <thibault402@hotmail.fr>
2014-11-06 14:06:42 +0100

    Validates references for uniqueness of doi per timeline

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -11,7 +11,8 @@ class Reference < ActiveRecord::Base
 
   attr_writer :current_step
 
-  validates_presence_of :title, :title_fr, :author, :year, :doi, :journal, :if => lambda { |o| o.current_step == "metadata" }
+  validates_presence_of :title, :title_fr, :author, :year, :doi, :url, :journal, :if => lambda { |o| o.current_step == "metadata" }
+  validates_uniqueness_of :timeline_id, :scope => [:doi], :if => lambda { |o| o.current_step == "metadata" }
 
   def current_step
     @current_step || steps.first
