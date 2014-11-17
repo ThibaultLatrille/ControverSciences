dfd0f55ed06ed3b93c7c09ce70eee8edd03e80fe
thibault <thibault402@hotmail.fr>
2014-11-18 00:40:18 +0100

    Added waiting_meliorations to user

--- db/migrate/20141117153406_add_pending_meliorations_to_users.rb
+++ db/migrate/20141117153406_add_pending_meliorations_to_users.rb
@@ -1,5 +1,6 @@
 class AddPendingMeliorationsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :pending_meliorations, :integer, default: 0
+    add_column :users, :waiting_meliorations, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -200,6 +200,7 @@ ActiveRecord::Schema.define(version: 20141117153406) do
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
     t.integer  "pending_meliorations", default: 0
+    t.integer  "waiting_meliorations", default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
