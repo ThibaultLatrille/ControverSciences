79de05051cc37a6ece089e6c0cc6f3d9374d8da0
thibault <thibault402@hotmail.fr>
2014-11-18 00:02:40 +0100

    After create update the pending_meliorations

--- app/models/melioration.rb
+++ app/models/melioration.rb
@@ -3,4 +3,12 @@ class Melioration < ActiveRecord::Base
   belongs_to :comment
 
   validates_uniqueness_of :user_id, :scope => [:comment_id]
+
+  after_create :update_user
+
+  private
+
+  def update_user
+    User.increment_counter(:pending_meliorations, self.to_user_id)
+  end
 end
