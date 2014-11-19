9fdcd0041372809050d41ad30103a295446e2374
thibault <thibault402@hotmail.fr>
2014-11-19 17:43:00 +0100

    Add support for tags in FollowingNewTimeline

--- app/controllers/following_new_timelines_controller.rb
+++ app/controllers/following_new_timelines_controller.rb
@@ -1,18 +1,29 @@
 class FollowingNewTimelinesController < ApplicationController
+
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    follow = FollowingNewTimeline.new( user_id: current_user.id)
-    if follow.save
-      flash[:success] = "Petit curieux, vous suivez maintenant toutes les nouvelles controverses"
+    if params[:tag]
+      tag = Tag.find_by_name(params[:tag])
+      follow = FollowingNewTimeline.new( user_id: current_user.id, tag_id: tag.id)
+      if follow.save
+        flash[:success] = "Petit curieux, vous suivez maintenant toutes les nouvelles controverses contenant le tag #{tags_hash[params[:tag]]}"
+      else
+        flash[:danger] = "Vous êtiez déjà attentif aux nouvelles controverses contenant le tag #{tags_hash[params[:tag]]}"
+      end
     else
-      flash[:danger] = "Vous êtes déjà attentif aux nouvelles controverses"
+      tag_ids = Tag.all.pluck(:id)
+      tag_ids.each do |tag_id|
+        follow = FollowingNewTimeline.new( user_id: current_user.id, tag_id: tag_id)
+        follow.save
+      end
+      flash[:success] = "Grand curieux, vous suivez maintenant toutes les nouvelles controverses"
     end
     redirect_to timelines_path
   end
 
   def destroy
-    fo = FollowingNewTimeline.find_by( user_id: current_user.id )
+    fo = FollowingNewTimeline.find_by( user_id: current_user.id, tag_id: params[:id] )
     unless fo.nil?
       fo.destroy
     end
--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,10 +2,11 @@ class FollowingsController < ApplicationController
   before_action :logged_in_user, only: [:index]
 
   def index
-    @fo_new_timelines = FollowingNewTimeline.find_by_user_id( current_user.id )
-    list_timelines = FollowingTimeline.where( user_id: current_user.id ).pluck( :timeline_id )
-    @fo_timelines = Timeline.where( id: list_timelines ).pluck( :id, :name )
-    list_references = FollowingReference.where( user_id: current_user.id ).pluck( :reference_id )
-    @fo_references = Reference.where( id: list_references ).pluck( :id, :title_fr )
+    tag_ids = FollowingNewTimeline.where( current_user.id ).pluck( :tag_id )
+    @fo_new_timelines = Tag.where( id: tag_ids ).pluck( :id, :name )
+    timeline_ids = FollowingTimeline.where( user_id: current_user.id ).pluck( :timeline_id )
+    @fo_timelines = Timeline.where( id: timeline_ids ).pluck( :id, :name )
+    reference_ids = FollowingReference.where( user_id: current_user.id ).pluck( :reference_id )
+    @fo_references = Reference.where( id: reference_ids ).pluck( :id, :title_fr )
   end
 end
