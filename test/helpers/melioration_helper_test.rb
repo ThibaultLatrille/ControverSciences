91ebe521b77792c4c12b7ba546a5b7d122067418
thibault <thibault402@hotmail.fr>
2014-11-21 11:51:28 +0100

    Changed melioration tag to fork which is more explicit

--- test/helpers/melioration_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MeliorationHelperTest < ActionView::TestCase
-end
--- test/models/melioration_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MeliorationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
