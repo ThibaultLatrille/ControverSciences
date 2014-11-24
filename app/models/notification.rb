fdcd35d2a7ff1e1181f4df47a031465e283b38da
thibault <thibault402@hotmail.fr>
2014-11-24 17:54:55 +0100

    Created attribute accessor

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,8 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
+
+  attr_accessor :timeline_ids
+  attr_accessor :reference_ids
+  attr_accessor :comment_ids
+  attr_accessor :sel_comment_ids
 end
