6a740226d54b11e197b857d718cfd12e8364b2fa
thibault <thibault402@hotmail.fr>
2014-11-26 00:25:08 +0100

    Changed ratings views from progress bar to emoticons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,12 @@ module ApplicationHelper
     {1 => "L'éxperience", 2 => "Les résultats", 3 => "Les limites",
      4 => "Quel rapport", 5 => "Remarques" }
   end
+
+  def star_hash
+    { 1 => "Je trouve qu'il rape un peu les fesses quand on se torche avec",
+      2 => "Heuresement pour eux que je n'étais pas un des reviewers",
+      3 => "Ils se sont quand même pas foulés trois neurones pour le pondre",
+      4 => "Des bons petits gars et du bon boulot",
+      5 => "J'ai eu un orgasme cérébrale en le lisant"}
+  end
 end
