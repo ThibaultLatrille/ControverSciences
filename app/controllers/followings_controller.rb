dfede6f9539d63a6d213a6ecf2390f84ad0f39f7
thibault <thibault402@hotmail.fr>
2014-11-25 10:30:12 +0100

    Refactored followings and added mouover for icons

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,10 +3,10 @@ class FollowingsController < ApplicationController
 
   def index
     tag_ids = FollowingNewTimeline.where( user_id: current_user.id ).pluck( :tag_id )
-    @fo_new_timelines = Tag.where( id: tag_ids ).pluck( :id, :name )
+    @fo_new_timelines = Tag.select(:id, :name).where( id: tag_ids )
     timeline_ids = FollowingTimeline.where( user_id: current_user.id ).pluck( :timeline_id )
-    @fo_timelines = Timeline.where( id: timeline_ids ).pluck( :id, :name )
+    @fo_timelines = Timeline.select(:id, :name ).where( id: timeline_ids )
     reference_ids = FollowingReference.where( user_id: current_user.id ).pluck( :reference_id )
-    @fo_references = Reference.where( id: reference_ids ).pluck( :id, :title_fr )
+    @fo_references = Reference.select( :id, :title_fr ).where( id: reference_ids )
   end
 end
