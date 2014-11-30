1ddec9f67e43c9f66ee560c563d6289d47a79d84
thibault <thibault402@hotmail.fr>
2014-11-30 01:06:10 +0100

    Created invitation controller

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
   get 'my_items/comments'
   get 'my_items/votes'
 
+  resources :invitations, only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
