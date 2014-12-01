013b19d90ee15cf682ab99b50b57e527818aedcc
thibault <thibault402@hotmail.fr>
2014-12-01 16:23:09 +0100

    Created invitation button in timeline show

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,15 @@
 class InvitationsController < ApplicationController
   def create
+    UserMailer.invitation({user_from: current_user,
+                          to: invitation_params[:email],
+                          message: invitation_params[:message],
+                          path: timeline_url(session[:timeline_id])}).deliver
+    render 'invitation/success'
+  end
 
+  private
+
+  def invitation_params
+    params.require( :invitation ).permit( :email, :message )
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,13 @@ class UserMailer < ActionMailer::Base
     mail to: user.email, subject: 'ControverSciences : Réinitialisation du mot de passe'
   end
 
+  def invitation( hash )
+    @user = hash[:user_from]
+    @message = hash[:message]
+    @path = hash[:path]
+    mail from: @user.email,
+         to: hash[:to],
+         subject: "Invitation sur ControverSciences de la part de #{@user.name}"
+  end
+
 end
