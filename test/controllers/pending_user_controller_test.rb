27c9955692869e10eebf82c6887af1bc2c70f008
thibault <thibault402@hotmail.fr>
2014-12-16 15:59:31 +0100

    Added the test for domains and pending users

---
+++ test/controllers/pending_user_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PendingUserControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/pending_user_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PendingUserHelperTest < ActionView::TestCase
+end
