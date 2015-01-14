969065e0011646d24986dd8d6dd54dd5e37f605a
thibault <thibault402@hotmail.fr>
2015-01-14 02:28:02 +0100

    Has many dependence for suggestions

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,8 +1,9 @@
 class Suggestion < ActiveRecord::Base
   belongs_to :user
-  has_many :suggestion_children
+  has_many :suggestion_children, dependent: :destroy
+  has_many :suggestion_votes, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 120 }
-  validates :comment, presence: true, length: {maximum: 480 }
+  validates :comment, presence: true, length: {maximum: 1200 }
 
 end
--- app/models/suggestion_child.rb
+++ app/models/suggestion_child.rb
@@ -1,9 +1,10 @@
 class SuggestionChild < ActiveRecord::Base
   belongs_to :user
   belongs_to :suggestion
+  has_many :suggestion_child_votes, dependent: :destroy
 
   validates :suggestion_id, presence: true
   validates :name, presence: true, length: { maximum: 120 }
-  validates :comment, presence: true, length: {maximum: 480 }
+  validates :comment, presence: true, length: {maximum: 1200 }
 
 end
