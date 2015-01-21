1e3dd2238e4b6ad83c9a22a3d6182f93bbcf1e66
thibault <thibault402@hotmail.fr>
2015-01-21 22:28:26 +0100

    Seed thibault lorin

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,12 @@ def seed_users
                admin: true,
                activated: true,
                activated_at: Time.zone.now)
+  users << User.new(name:  "T. Lorin",
+                    email: "thibault.lorin@ens-lyon.fr",
+                    password:              "password",
+                    password_confirmation: "password",
+                    activated: true,
+                    activated_at: Time.zone.now)
   30.times do |n|
     first_name  = Faker::Name.first_name
     last_name  = Faker::Name.last_name
