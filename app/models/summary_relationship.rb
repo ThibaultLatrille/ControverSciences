b1dc252f6d4319265fee88b9bc0383e5360d99d8
thibault <thibault402@hotmail.fr>
2015-02-27 00:23:05 +0100

    Good trough table

--- app/models/summary_relationship.rb
+++ app/models/summary_relationship.rb
@@ -1,6 +1,6 @@
 class SummaryRelationship < ActiveRecord::Base
-  belongs_to :parent, class_name: "User"
-  belongs_to :child, class_name: "User"
+  belongs_to :parent, class_name: "Summary", foreign_key: "parent_id"
+  belongs_to :child, class_name: "Summary", foreign_key: "child_id"
   validates :parent_id, presence: true
   validates :child_id, presence: true, uniqueness: true
 end
