02b591f52001dfa05b5b9a308e824162a2c1547e
thibault <thibault402@hotmail.fr>
2015-04-21 14:36:46 +0200

    Added pages for empty contributions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,17 @@ class StaticPagesController < ApplicationController
   def home
     @timelines = Timeline.order(:score => :desc).first(8)
   end
+
+  def empty_comments
+    @references = Reference.select(:id, :title, :timeline_id, :created_at).order(:created_at => :desc).where( title_fr: nil )
+    puts @references
+  end
+
+  def empty_summaries
+    @timelines = Timeline.select(:id, :name, :created_at).order(:created_at => :desc).where( nb_summaries: 0 )
+  end
+
+  def empty_references
+    @timelines = Timeline.select(:id, :name, :created_at).order(:created_at => :desc).where( nb_references: 0 )
+  end
 end
