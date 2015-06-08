f9b3b7e3ba24c0a0bcce5603020f729c623a9819
thibault <thibault402@hotmail.fr>
2015-06-09 00:36:27 +0200

    Show timelines that you like

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,38 +4,41 @@ class TimelinesController < ApplicationController
   def index
     query = Timeline.order(params[:sort].blank? ? :score : params[:sort].to_sym =>
                                params[:order].blank? ? :desc : params[:order].to_sym)
-    if params[:tag] != 'all' && !params[:tag].blank?
+    unless params[:tag].blank? || params[:tag] == 'all'
       query = query.tagged_with(params[:tag])
     end
     unless params[:filter].blank?
       query = query.search_by_name(params[:filter])
     end
+    if !params[:interest].blank? && logged_in?
+      query = query.where(id: Like.where(user_id: current_user.id).pluck(:timeline_id))
+    end
     @timelines = query.page(params[:page]).per(24)
     if logged_in?
-      @my_likes = Like.where(user_id: current_user.id).pluck( :timeline_id )
+      @my_likes = Like.where(user_id: current_user.id).pluck(:timeline_id)
     end
   end
 
   def new
-    @timeline = Timeline.new
+    @timeline        = Timeline.new
     @timeline.binary = true
-    @tag_list = []
+    @tag_list        = []
   end
 
   def edit
-    @timeline = Timeline.find( params[:id] )
+    @timeline = Timeline.find(params[:id])
     @tag_list = @timeline.get_tag_list
     if @timeline.binary != ""
-      @timeline.binary_left = @timeline.binary.split('&&')[0]
+      @timeline.binary_left  = @timeline.binary.split('&&')[0]
       @timeline.binary_right = @timeline.binary.split('&&')[1]
-      @timeline.binary = true
+      @timeline.binary       = true
     else
       @timeline.binary = false
     end
   end
 
   def update
-    @timeline = Timeline.find( params[:id] )
+    @timeline = Timeline.find(params[:id])
     if @timeline.user_id == current_user.id
       @timeline.name = timeline_params[:name]
       if timeline_params[:binary] != "0"
@@ -47,13 +50,13 @@ class TimelinesController < ApplicationController
         @timeline.set_tag_list(params[:timeline][:tag_list])
       end
       if @timeline.save
-          flash[:success] = "Controverse modifiée."
-          redirect_to @timeline
+        flash[:success] = "Controverse modifiée."
+        redirect_to @timeline
       else
-        @tag_list = @timeline.get_tag_list
-        @timeline.binary_left = @timeline.binary.split('&&')[0]
+        @tag_list              = @timeline.get_tag_list
+        @timeline.binary_left  = @timeline.binary.split('&&')[0]
         @timeline.binary_right = @timeline.binary.split('&&')[1]
-        @timeline.binary = true
+        @timeline.binary       = true
         render 'edit'
       end
     else
@@ -62,7 +65,7 @@ class TimelinesController < ApplicationController
   end
 
   def create
-    @timeline = Timeline.new( user_id: current_user.id, name: timeline_params[:name], debate: true)
+    @timeline = Timeline.new(user_id: current_user.id, name: timeline_params[:name], debate: true)
     if timeline_params[:binary] == "1"
       @timeline.binary = "#{timeline_params[:binary_left].strip}&&#{timeline_params[:binary_right].strip}"
     else
@@ -72,14 +75,14 @@ class TimelinesController < ApplicationController
       @timeline.set_tag_list(params[:timeline][:tag_list])
     end
     if @timeline.save
-        flash[:success] = "Controverse ajoutée !"
-        redirect_to @timeline
+      flash[:success] = "Controverse ajoutée !"
+      redirect_to @timeline
     else
       @tag_list = @timeline.get_tag_list
       if @timeline.binary != ""
-        @timeline.binary_left = @timeline.binary.split('&&')[0]
+        @timeline.binary_left  = @timeline.binary.split('&&')[0]
         @timeline.binary_right = @timeline.binary.split('&&')[1]
-        @timeline.binary = true
+        @timeline.binary       = true
       else
         @timeline.binary = false
       end
@@ -88,19 +91,19 @@ class TimelinesController < ApplicationController
   end
 
   def show
-    @timeline = Timeline.find(params[:id])
+    @timeline    = Timeline.find(params[:id])
     summary_best = SummaryBest.find_by(timeline_id: params[:id])
     if summary_best
-      @summary = Summary.find( summary_best.summary_id )
+      @summary = Summary.find(summary_best.summary_id)
     else
       @summary = nil
     end
     if logged_in?
-      @my_likes = Like.where(user_id: current_user.id).pluck( :timeline_id )
-      @improve = Summary.where(user_id: current_user.id, timeline_id: params[:id]).count == 1 ? false : true
+      @my_like = Like.find_by(user_id: current_user.id, timeline_id: params[:id])
+      @improve  = Summary.where(user_id: current_user.id, timeline_id: params[:id]).count == 1 ? false : true
     end
-    @titles = Reference.where( timeline_id: @timeline.id, title_fr: [nil, ""] ).count
-    @references = Reference.select( :article, :id, :title_fr, :title, :year, :binary_most, :star_most, :nb_edits).order( year: :desc).where( timeline_id: @timeline.id )
+    @titles     = Reference.where(timeline_id: @timeline.id, title_fr: [nil, ""]).count
+    @references = Reference.select(:article, :id, :title_fr, :title, :year, :binary_most, :star_most, :nb_edits).order(year: :desc).where(timeline_id: @timeline.id)
   end
 
   def destroy
