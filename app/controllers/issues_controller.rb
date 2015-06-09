1760fb343c024a1e0f9e2caa633f1bb60e130294
thibault <thibault402@hotmail.fr>
2015-06-09 23:59:35 +0200

    Send issue event when lable are not set

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,13 @@
 class IssuesController < ApplicationController
   def create
     issue = Issue.new(issue_params)
-    issue.labels = params[:issue][:labels] << params[:issue][:importance]
+    issue.labels = params[:issue][:labels].blank? ? ["unset-label"] : params[:issue][:labels]
+    if params[:issue][:importance].blank?
+      issue.labels << "unset-importance"
+    else
+      issue.labels << params[:issue][:importance]
+    end
+    issue.title.blank? ? issue.title = "Unset" : nil
     issue.body += "\n Sur la page " + issue.url + "\n Par : " + issue.author + "\n Navigateur : " + browser.to_s
     if issue.save
       respond_to do |format|
