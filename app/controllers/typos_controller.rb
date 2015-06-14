c596410ebb371971983aa2677e503547b23c49f3
thibault <thibault402@hotmail.fr>
2015-06-14 18:02:26 +0200

    Modal for typos

--- app/controllers/typos_controller.rb
+++ app/controllers/typos_controller.rb
@@ -2,13 +2,13 @@ class TyposController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
 
   def new
-    @typo = Type.new( typo_params )
+    @typo = Typo.new( get_params )
     if !@typo.comment_id.blank?
-      com = Comment.find(typo_params[:comment_id])
+      com = Comment.find(get_params[:comment_id])
       @typo.content = com.field_content(typo_params[:field])
       @typo.target_user_id = com.user_id
     elsif !@typo.summary_id.blank?
-      sum = Summary.select( :user_id, :content ).find(typo_params[:summary_id])
+      sum = Summary.select( :user_id, :content ).find(get_params[:summary_id])
       @typo.content = sum.content
       @typo.target_user_id = sum.user_id
     end
@@ -19,14 +19,14 @@ class TyposController < ApplicationController
   end
 
   def create
-    @typo = Type.new( typo_params )
+    @typo = Typo.new( typo_params )
     if !@typo.comment_id.blank?
       @typo.target_user_id = Comment.select( :user_id ).find(typo_params[:comment_id]).user_id
     elsif !@typo.summary_id.blank?
       @typo.target_user_id = Summary.select( :user_id ).find(typo_params[:summary_id]).user_id
     end
     @typo.user_id = current_user.id
-    if type.save
+    if @typo.save
       respond_to do |format|
         format.js { render 'typos/success', :content_type => 'text/javascript', :layout => false}
       end
@@ -38,6 +38,10 @@ class TyposController < ApplicationController
   end
 
   private
+
+  def get_params
+    params.permit(:comment_id, :summary_id, :field)
+  end
 
   def typo_params
     params.require(:typo).permit(:comment_id, :summary_id, :content, :field)
