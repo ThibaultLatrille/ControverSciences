3acbbd836b69e36755a76124708a398544711a92
thibault <thibault402@hotmail.fr>
2015-06-15 16:00:03 +0200

    Added logged_in

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,4 @@
 Rails.application.config.assets.version = '2.1'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.conf*ig.assets.precompile += %w( search.js )
