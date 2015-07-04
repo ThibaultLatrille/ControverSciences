56f364ddbd1bce6f30d4752e18bcd851e99cdf64
thibault <thibault402@hotmail.fr>
2015-07-04 10:40:49 +0200

    link to your empty references and summaries

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,24 +2,42 @@ class StaticPagesController < ApplicationController
   def home
     @timelines = Timeline.order(:score => :desc).first(8)
     if logged_in?
-      @my_likes = Like.where(user_id: current_user.id).pluck( :timeline_id )
+      @my_likes = Like.where(user_id: current_user.id).pluck(:timeline_id)
     end
   end
 
   def empty_comments
+    query = Reference.select(:id, :title, :timeline_id, :created_at)
+                    .order(:created_at => :desc)
+                    .where(title_fr: nil)
     if params[:filter] == "mine" && logged_in?
-      @references = Reference.select(:id, :title, :timeline_id, :created_at).order(:created_at => :desc).where( title_fr: nil, user_id: current_user.id )
+      @references = query.where(user_id: current_user.id)
     else
-      @references = Reference.select(:id, :title, :timeline_id, :created_at).order(:created_at => :desc).where( title_fr: nil )
+      @references = query
     end
   end
 
   def empty_summaries
-    @timelines = Timeline.select(:id, :name, :created_at).order(:created_at => :desc).where( nb_summaries: 0 ).where.not( nb_references: 0..3 )
+    query = Timeline.select(:id, :name, :created_at)
+                    .order(:created_at => :desc)
+                    .where(nb_summaries: 0)
+                    .where.not(nb_references: 0..3)
+    if params[:filter] == "mine" && logged_in?
+      @timelines = query.where(user_id: current_user.id)
+    else
+      @timelines = query
+    end
   end
 
   def empty_references
-    @timelines = Timeline.select(:id, :name, :created_at).order(:created_at => :desc).where( nb_references: 0..3 )
+    query = Timeline.select(:id, :name, :created_at)
+                    .order(:created_at => :desc)
+                    .where(nb_references: 0..3)
+    if params[:filter] == "mine" && logged_in?
+      @timelines = query.where(user_id: current_user.id)
+    else
+      @timelines = query
+    end
   end
 
   def newsletter
