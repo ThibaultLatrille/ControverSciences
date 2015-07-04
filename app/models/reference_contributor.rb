f2469200eda720f81bc497adf8908273b3bb928e
thibault <thibault402@hotmail.fr>
2015-07-04 10:28:05 +0200

    Timeline contributor and reference contributor behave correctly

--- app/models/reference_contributor.rb
+++ app/models/reference_contributor.rb
@@ -3,10 +3,16 @@ class ReferenceContributor < ActiveRecord::Base
   belongs_to :reference
   validates :user_id, presence: true
   validates :reference_id, presence: true
+  validates_uniqueness_of :user_id, :scope => [:reference_id]
 
   after_create :cascading_create
+  after_destroy :cascading_destroy
 
   def cascading_create
     Reference.increment_counter(:nb_contributors, self.reference_id)
+  end
+
+  def cascading_destroy
+    Reference.decrement_counter(:nb_contributors, self.reference_id)
   end
 end
--- app/models/timeline_contributor.rb
+++ app/models/timeline_contributor.rb
@@ -3,10 +3,16 @@ class TimelineContributor < ActiveRecord::Base
   belongs_to :timeline
   validates :user_id, presence: true
   validates :timeline_id, presence: true
+  validates_uniqueness_of :user_id, :scope => [:timeline_id]
 
   after_create :cascading_create
+  after_destroy :cascading_destroy
 
   def cascading_create
     Timeline.increment_counter(:nb_contributors, self.timeline_id)
+  end
+
+  def cascading_destroy
+    Timeline.decrement_counter(:nb_contributors, self.timeline_id)
   end
 end
