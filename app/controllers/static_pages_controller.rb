8318d9f71c745b30f831a2f06f1b624d0d97428f
thibault <thibault402@hotmail.fr>
2015-07-06 12:40:18 +0200

    If logged_in, empty shows only interests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,11 +10,14 @@ class StaticPagesController < ApplicationController
     query = Reference.select(:id, :title, :timeline_id, :created_at)
                     .order(:created_at => :desc)
                     .where(title_fr: nil)
-    if params[:filter] == "mine" && logged_in?
-      @references = query.where(user_id: current_user.id)
-    else
-      @references = query
+    if logged_in?
+      if params[:filter] == "mine"
+        query = query.where(user_id: current_user.id)
+      else
+        query = query.where( timeline_id: Like.where(user_id: current_user.id).pluck(:timeline_id))
+      end
     end
+    @references = query
   end
 
   def empty_summaries
@@ -23,22 +26,28 @@ class StaticPagesController < ApplicationController
                     .where(nb_summaries: 0)
                     .where.not(nb_references: 0..3)
                     .where.not(nb_comments: 0..3)
-    if params[:filter] == "mine" && logged_in?
-      @timelines = query.where(user_id: current_user.id)
-    else
-      @timelines = query
+    if logged_in?
+      if params[:filter] == "mine"
+        query = query.where(user_id: current_user.id)
+      else
+        query = query.where( id: Like.where(user_id: current_user.id).pluck(:timeline_id))
+      end
     end
+    @timelines = query
   end
 
   def empty_references
     query = Timeline.select(:id, :name, :created_at)
                     .order(:created_at => :desc)
                     .where(nb_references: 0..3)
-    if params[:filter] == "mine" && logged_in?
-      @timelines = query.where(user_id: current_user.id)
-    else
-      @timelines = query
+    if logged_in?
+      if params[:filter] == "mine"
+        query = query.where(user_id: current_user.id)
+      else
+        query = query.where( id: Like.where(user_id: current_user.id).pluck(:timeline_id))
+      end
     end
+    @timelines = query
   end
 
   def newsletter
