b18a27bb4372548abb283b845664c0c32c201918
thibault <thibault402@hotmail.fr>
2015-07-20 00:15:07 +0200

    Colors and hide grap for contributions

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,9 +1,23 @@
 class ContributionsController < ApplicationController
   def index
-    @contrib_timelines = Timeline.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    @contrib_references = Reference.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    @contrib_users = User.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    @contrib_comments = Comment.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    @contrib_summaries = Summary.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    @contribs = {0 => [], 1 => [], 2 => [], 3 => [], 4 => []}
+    temp_contribs = {}
+    temp_contribs[0] = Timeline.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    temp_contribs[1] = Reference.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    temp_contribs[2] = User.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    temp_contribs[3] = Comment.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    temp_contribs[4] = Summary.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    @keys = temp_contribs.values.map{ |x| x.keys }.flatten.uniq[0..-2]
+    (0..4).each do |contrib|
+      sum = 0
+      @keys.each do |key|
+        if params[:filter] == "freq"
+          sum = temp_contribs[contrib][key].blank? ? 0 : temp_contribs[contrib][key].length
+        else
+          sum += temp_contribs[contrib][key].blank? ? 0 : temp_contribs[contrib][key].length
+        end
+        @contribs[contrib] << sum
+      end
+    end
   end
 end
