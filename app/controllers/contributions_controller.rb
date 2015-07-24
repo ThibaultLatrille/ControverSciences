d7795687f6d26b89a55d037c61ef5341bf2a0a4e
thibault <thibault402@hotmail.fr>
2015-07-24 10:36:12 +0200

    Localize datetime for contributions

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -2,11 +2,11 @@ class ContributionsController < ApplicationController
   def index
     @contribs = {0 => [], 1 => [], 2 => [], 3 => [], 4 => []}
     temp_contribs = {}
-    temp_contribs[0] = User.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    temp_contribs[1] = Timeline.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    temp_contribs[2] = Reference.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    temp_contribs[3] = Comment.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
-    temp_contribs[4] = Summary.all.pluck(:created_at).group_by{ | date| date.strftime("%B, %Y") }
+    temp_contribs[0] = User.order( created_at: :asc ).all.pluck(:created_at).group_by{ | date| I18n.l date.to_date, format: :month }
+    temp_contribs[1] = Timeline.order( created_at: :asc ).all.pluck(:created_at).group_by{ | date| I18n.l date.to_date, format: :month }
+    temp_contribs[2] = Reference.order( created_at: :asc ).all.pluck(:created_at).group_by{ | date| I18n.l date.to_date, format: :month }
+    temp_contribs[3] = Comment.order( created_at: :asc ).all.pluck(:created_at).group_by{ | date| I18n.l date.to_date, format: :month }
+    temp_contribs[4] = Summary.order( created_at: :asc ).all.pluck(:created_at).group_by{ | date| I18n.l date.to_date, format: :month }
     @keys = temp_contribs.values.map{ |x| x.keys }.flatten.uniq[0..-2]
     (0..4).each do |contrib|
       sum = 0
