a1492a5681ff0fc7507c92ba6a91e6bf3f171baa
thibault <thibault402@hotmail.fr>
2015-08-23 19:45:43 +0200

    Slack controller was inadventertly deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def new
     @user = User.new
@@ -19,17 +19,17 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.includes(:user_detail).where( activated: true ).order(score: :desc ).page(params[:page]).per(24)
+    @users = User.includes(:user_detail).where(activated: true).order(score: :desc).page(params[:page]).per(24)
   end
 
   def show
     begin
-      @user = User.find(params[:id])
+      @user        = User.find(params[:id])
       @user_detail = @user.user_detail
-      @timelines = Timeline.includes(:tags).select(:id, :slug, :name).where(user_id: @user.id).where.not(private: true)
-      @references = Reference.select(:id, :slug, :timeline_id, :title).where(user_id: @user.id)
-      @comments = Comment.select(:id, :reference_id, :title_markdown ).where(user_id: @user.id).where(public: true)
-      @summaries = Summary.select(:id, :timeline_id, :content ).where(user_id: @user.id).where(public: true)
+      @timelines   = Timeline.includes(:tags).select(:id, :slug, :name).where(user_id: @user.id).where.not(private: true)
+      @references  = Reference.select(:id, :slug, :timeline_id, :title).where(user_id: @user.id)
+      @comments    = Comment.select(:id, :reference_id, :title_markdown).where(user_id: @user.id).where(public: true)
+      @summaries   = Summary.select(:id, :timeline_id, :content).where(user_id: @user.id).where(public: true)
     rescue ActiveRecord::RecordNotFound
       flash[:danger] = "Aucun contributeur à ce lien ;-("
       redirect_to users_path
@@ -37,10 +37,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-    @user_detail = UserDetail.find_by_user_id( params[:id] )
+    @user        = User.find(params[:id])
+    @user_detail = UserDetail.find_by_user_id(params[:id])
     unless @user_detail
-      @user_detail = UserDetail.new( user_id: params[:id] )
+      @user_detail            = UserDetail.new(user_id: params[:id])
       @user_detail.send_email = true
     end
   end
@@ -49,22 +49,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       if logged_in?
-        @my_likes = Like.where(user_id: current_user.id).pluck( :timeline_id )
+        @my_likes = Like.where(user_id: current_user.id).pluck(:timeline_id)
       end
       if logged_in?
-        @my_likes = Like.where(user_id: current_user.id).pluck( :timeline_id )
+        @my_likes = Like.where(user_id: current_user.id).pluck(:timeline_id)
       end
       @timelines = Timeline.order(:score => :desc).first(8)
       if @user.invalid_email
-        PendingUser.create( user_id: @user.id, why: user_params[:why])
+        PendingUser.create(user_id: @user.id, why: user_params[:why])
         render 'users/invalid'
       else
         mg_client = Mailgun::Client.new ENV['MAILGUN_CS_API']
-        message = {
-            :subject=> t('controllers.activation_email'),
-            :from=>"activation@controversciences.org",
-            :to => @user.email,
-            :html => render_to_string( :file => 'user_mailer/account_activation', layout: nil ).to_str
+        message   = {
+            :subject => t('controllers.activation_email'),
+            :from    => "activation@controversciences.org",
+            :to      => @user.email,
+            :html    => render_to_string(:file => 'user_mailer/account_activation', layout: nil).to_str
         }
         mg_client.send_message "controversciences.org", message
         render 'users/success'
@@ -80,9 +80,9 @@ class UsersController < ApplicationController
       flash[:success] = t('controllers.user_updated')
       redirect_to @user
     else
-      @user_detail = UserDetail.find_by_user_id( params[:id] )
+      @user_detail = UserDetail.find_by_user_id(params[:id])
       unless @user_detail
-        @user_detail = UserDetail.new( user_id: params[:id] )
+        @user_detail            = UserDetail.new(user_id: params[:id])
         @user_detail.send_email = true
       end
       render 'edit'
@@ -101,17 +101,37 @@ class UsersController < ApplicationController
   end
 
   def slack
-    Slack.configure do |config|
-      config.token = ENV['SLACK_API_TOKEN']
-    end
-    client = Slack::Web::Client.new
+    if Rails.env.production?
+      Slack.configure do |config|
+        config.token = ENV['SLACK_API_TOKEN']
+      end
+      client = Slack::Web::Client.new
 
-    if client.auth_test
-      flash[:danger] = t('controllers.slack_already_invited', email: current_user.email)
-      redirect_to current_user
+      if client.auth_test
+        if client.users_list["members"].index { |user| user["profile"]["email"] == current_user.email }.blank?
+          if ["ens-lyon.fr", "umontpellier.fr", "controversciences.org", "univ-montp2.fr"].include? current_user.email.partition("@")[2]
+            redirect_to "https://controversciences.slack.com/signup"
+          else
+            begin
+              client.post('users.admin.invite', {email: current_user.email, set_active: true})
+              flash[:success] = t('controllers.slack_invit', email: current_user.email)
+              redirect_to :back
+            rescue
+              admin_group = client.groups_list['groups'].detect { |c| c['name'] == 'admins' }
+              client.chat_postMessage(channel: admin_group['id'], text: "#{current_user.email} invitation needs to be resent !")
+              flash[:danger] = t('controllers.slack_already_invited', email: current_user.email)
+              redirect_to :back
+            end
+          end
+        else
+          redirect_to "https://controversciences.slack.com/signin"
+        end
+      else
+        flash[:danger] = t('controllers.slack_connexion_lost')
+        redirect_to :back
+      end
     else
-      flash[:danger] = t('controllers.slack_connexion_lost')
-      redirect_to current_user
+      redirect_to "https://controversciences.slack.com/signup"
     end
   end
 
@@ -122,8 +142,8 @@ class UsersController < ApplicationController
   end
 
   def previous
-    users = User.select(:id, :slug, :score).order(score: :desc).where( activated: true )
-    i = users.index{|x| x.id == params[:id].to_i }
+    users = User.select(:id, :slug, :score).order(score: :desc).where(activated: true)
+    i     = users.index { |x| x.id == params[:id].to_i }
     if i == 0
       i = users.length-1
     else
@@ -133,8 +153,8 @@ class UsersController < ApplicationController
   end
 
   def next
-    users = User.select(:id, :slug, :score).order(score: :desc).where( activated: true )
-    i = users.index{|x| x.id == params[:id].to_i }
+    users = User.select(:id, :slug, :score).order(score: :desc).where(activated: true)
+    i     = users.index { |x| x.id == params[:id].to_i }
     if i == users.length-1
       i = 0
     else
