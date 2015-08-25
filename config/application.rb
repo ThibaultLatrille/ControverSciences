d08f6791e6c21262841d605991acad0b5d873577
thibault <thibault402@hotmail.fr>
2015-08-25 14:18:21 +0200

    Enforce SSL

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SampleApp
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
-    config.force_ssl = false
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
