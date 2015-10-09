f48e561faa1667b5e08eb1263a0a6f80fb1719bf
thibault <thibault402@hotmail.fr>
2015-10-09 11:51:14 +0000

    Contributors are added to the mailing list upon activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,6 +4,10 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
+      if Rails.env.production?
+        mg_client = Mailgun::Client.new ENV['MAILGUN_CS_API']
+        mg_client.post("/lists/contributeurs@controversciences.org/members", {address: user.email})
+      end
       flash[:success] = t('controllers.account_activated')
       log_in user
       redirect_to user
