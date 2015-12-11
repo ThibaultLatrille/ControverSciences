718d05e3ad982c27545e1492a5c14d33e8370559
thibault <thibault402@hotmail.fr>
2015-12-11 16:20:01 +0000

    Added Voronoi

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,4 @@ Rails.application.config.assets.version = '4.5'
 # Rails.application.conf*ig.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( chart.js )
 Rails.application.config.assets.precompile += %w( not_logged.js )
+Rails.application.config.assets.precompile += %w( voronoi.js )
