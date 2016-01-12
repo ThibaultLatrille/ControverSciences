71b59cd13b606b6c3fc50729c9f1081d0663f557
thibault <thibault402@hotmail.fr>
2016-01-12 17:09:12 +0000

    Updated the counter for number of characters max, using regexp to substract special characters and links (front-end and back-end). Added debouncer in front-end

--- lib/HTMLlinks.rb
+++ lib/HTMLlinks.rb
@@ -12,6 +12,7 @@ class HTMLlinks < Redcarpet::Render::HTML
   attr_accessor :links
   attr_accessor :counter
   attr_accessor :root_url
+
   def link(link, title, content)
     if link !~ /\D/ && !link.blank? && link != "0"
       if content == "*"
@@ -47,6 +48,11 @@ end
 
 class String
   def length_sub
-    self.gsub("\r\n"," ").length
+    self.gsub(/\[\*\]\([0-9]*\)/, '')
+        .gsub(/\]\(\/timelines\/[0-9]*\)/, '')
+        .gsub(/\]\((?:(?:https?|\(ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9]+-?)*[a-z0-9]+)(?:\.(?:[a-z0-9]+-?)*[a-z0-9]+)*(?:\.(?:[a-z]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\)/, '')
+        .gsub(/\*|\^|_|\(|\)|\[|\]|-/, '')
+        .gsub(/[0-9]\. /, '')
+        .gsub("\r\n", " ").length
   end
 end
