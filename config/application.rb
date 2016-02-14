ebb8e608eb1f2d670ce67c6c237724296826783e
thibault <thibault402@hotmail.fr>
2016-02-14 14:34:04 +0000

    Fidex typo

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
+    config.active_record.schema_format = :sql
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
