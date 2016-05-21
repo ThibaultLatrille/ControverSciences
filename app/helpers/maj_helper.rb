8dd8941a086765cd288868c7b81275f8d347d550
Thibault Latrille <thibault.latrille@ens-lyon.org>
2016-05-21 13:04:08 +0200

    Maj counter (nb_references and nb_comments) for timelines who is fucked up

--- app/helpers/maj_helper.rb
+++ app/helpers/maj_helper.rb
@@ -1,4 +1,14 @@
 module MajHelper
+
+  def maj_counter
+    ActiveRecord::Base.transaction do
+      Timeline.find_each do |timeline|
+        timeline.update_columns(nb_comments: Comment.where(timeline_id: timeline.id).count,
+                                nb_references: Reference.where(timeline_id: timeline.id).where.not(title_fr: '').count)
+      end
+    end
+  end
+
   def maj_v_14
     ActiveRecord::Base.transaction do
       UserDetail.find_each do |user_detail|
@@ -12,7 +22,7 @@ module MajHelper
 
   def maj_v_13
     ActiveRecord::Base.transaction do
-      Timeline.where.not(binary: "").find_each do |timeline|
+      Timeline.where.not(binary: '').find_each do |timeline|
         Binary.where(timeline_id: timeline.id).update_all(frame_id: Frame.select(:id).find_by(timeline_id: timeline.id, best: true).id )
       end
     end
