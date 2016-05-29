cbf9682091873fbd6666a678a23d9d2fa3b4b7c3
Thibault Latrille <thibault.latrille@ens-lyon.org>
2016-05-29 12:15:38 +0200

    Exclusion of domain from well known to be not acceptable

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,7 @@
 class Domain < ActiveRecord::Base
+  validates :name, :exclusion => { :in => ["aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
+                                           "google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com",
+                                           "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
+                                           "hotmail.fr", "live.fr", "laposte.net", "yahoo.fr", "wanadoo.fr", "orange.fr", "gmx.fr", "sfr.fr", "neuf.fr", "free.fr"],
+                                        :message => "Subdomain %{value} is reserved." }
 end
