dd33826b2c74a3dc39797504dadfc93bb9a901f6
Thibault Latrille <thibault.latrille@ens-lyon.org>
2016-06-23 19:01:45 +0200

    Helper to set binary properly for all references

--- app/helpers/maj_helper.rb
+++ app/helpers/maj_helper.rb
@@ -1,15 +1,4 @@
 module MajHelper
-
-  def maj_v_15
-    ActiveRecord::Base.transaction do
-      score = 0
-      Question.all.each do |question|
-        question.update_attribute(:score, score)
-        score += 1
-      end
-    end
-  end
-
   def maj_counter
     ActiveRecord::Base.transaction do
       Timeline.find_each do |timeline|
@@ -27,23 +16,9 @@ module MajHelper
     end
   end
 
-
-  def maj_v_14
+  def maj_update_binary
     ActiveRecord::Base.transaction do
-      UserDetail.find_each do |user_detail|
-        unless user_detail.send_email
-          user_detail.frequency = 0
-          user_detail.save
-        end
-      end
-    end
-  end
-
-  def maj_v_13
-    ActiveRecord::Base.transaction do
-      Timeline.where.not(binary: '').find_each do |timeline|
-        Binary.where(timeline_id: timeline.id).update_all(frame_id: Frame.select(:id).find_by(timeline_id: timeline.id, best: true).id )
-      end
+      Timeline.find_each.map{|t| t.references.each{ |r| r.update_columns(binary: t.binary)}}
     end
   end
 end
