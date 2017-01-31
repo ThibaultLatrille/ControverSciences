5b0ce5bab72b76a5c32ac6a2672bcd6d9f484d62
Thibault Latrille <thibault.latrille@ens-lyon.org>
2017-01-31 15:16:08 +0100

    New config for errors raising

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-
+    config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
