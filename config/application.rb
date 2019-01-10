4192df5dd13dfbf20834805ff967dfcccbe9df27
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2019-01-11 00:21:41 +0100

    String redifinition at initialization

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module SampleApp
       require 'carrierwave'
     end
 
+    require 'ext/string'
+
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
